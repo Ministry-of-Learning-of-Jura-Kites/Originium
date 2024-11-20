ad1a8d6c644c6d77307dd3cf02fdfaa6ff157c61
Phudit-2547 <bigphudit2547@gmail.com>
2024-11-20 09:00:31 +0000

    observation

--- scratchpads/illusory_shield.py
+++ scratchpads/illusory_shield.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from mage_ai.io.file import FileIO
 
-filepath = "/home/src/Downloads/Data 2018-2023/Project/2023/202302889.json"
-df = pd.DataFrame(FileIO().load(filepath))
+filepath = "/home/src/Downloads/Data 2018-2023/Project/2023/202302889"
+df = pd.DataFrame(FileIO().load(filepath,'json'))
 print(df)
